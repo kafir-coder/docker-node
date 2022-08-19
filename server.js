08a0a5cfc4102f07498270736c01b27ab01d207e
Caio Tony <caiotony16@gmail.com>
2022-08-19 22:52:31 +0100

    chore: change from ts to js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+
+const PORT = process.env.PORT
+
+const app = express()
+
+
+app.get("/ping", (req, res) => {
+    res.status(200).send("pong")
+})
+app.listen(PORT, () => {
+    console.log(`server listenning on port ${PORT}`)
+})
